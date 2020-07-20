0f9bc2af5e1c2c68bcc1223072230c5734137a79
Aleksei Tolkachev <bear_pack@mail.ru>
2020-07-20 16:19:25 +0800

    Fix base url and change project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Smartapartmentdata
+# Test angular app
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.21.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/smartapartmentdata'),
+      dir: require('path').join(__dirname, './coverage/testangularapp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
